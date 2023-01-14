c47e601f82a2212afb5562027b0cc9988824c06c
cecelia001 <diascece@gmail.com>
2023-01-14 17:44:00 -0500

    new styling

---
+++ AboutMe.js
@@ -0,0 +1,13 @@
+function aboutMe(theObject) {
+    theObject.nickname = 'Cece';
+  }
+  
+  const myfavorites = {
+    drink: 'coffee',
+    program: 'JavaScript',
+    backend: 'Node.js',
+    frontend: 'React',
+    fullstack: true,
+    quality: 'humor'
+  };
+  
